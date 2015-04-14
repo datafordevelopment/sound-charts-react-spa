38da87ee118c0fd58d825ca5e2371f6b191bf781
Nazar Aziz <mcnazar@gmail.com>
2015-04-14 15:43:52 +0100

    vagrant with privision script to install node via nvm

---
+++ provision.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+
+# install dependencies
+sudo apt-get update
+sudo apt-get install -y git-core curl
+
+# install node via nvm -  https://github.com/creationix/nvm
+echo "Installing nvm..."
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+
+nvm install 0.12.2
+nvm alias default 0.12.2
