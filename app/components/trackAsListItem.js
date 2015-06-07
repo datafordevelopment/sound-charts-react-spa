4c9703499d18f82c2fd8dbccfdf2559253f7d06d
Nazar Aziz <mcnazar@gmail.com>
2015-06-07 15:23:28 +0100

    refs #1 - improve list layout

--- app/components/trackAsListItem.js
+++ app/components/trackAsListItem.js
@@ -23,8 +23,7 @@ export default React.createClass( {
         }
 
         trackClasses = cx(
-            'track-as-list-item-container',
-            'col-md-12',
+            'thumbnail',
             {
                 active: this.props.active
             }
@@ -33,40 +32,61 @@ export default React.createClass( {
         return (
             <div className="track-as-list-item">
                <div className={trackClasses}
-                    onClick={this.toggleTrackPlay}
                    >
-                   <div className="thumbnail">
-                       <div className="row">
-                           <div className="col-md-1 col-sm-2 col-xs-2 album-thumbnail-container">
-                               <div className="thumbnail">
-                                   {track.image_url && <img src={track.image_url} alt=""/>}
-                                   {!track.image_url && <img src={require('assets/img/no-image.jpg')}/>}
+                   <div className="logo t-cell">
+                       {track.image_url && <img src={track.image_url} alt=""/>}
+                       {!track.image_url && <img src={require('assets/img/no-image.jpg')}/>}
+                   </div>
+
+                   <div className="details t-cell">
+                       <div className="t-row">
+                           <div className="track-details-container">
+                               <div className="play-indicator t-cell" onClick={this.toggleTrackPlay}>
+                                   <span className="fa-stack fa-lg">
+                                       <i className="fa fa-circle fa-stack-1x"></i>
+                                       { playerControl }
+                                   </span>
                                </div>
+                               <div className="sub-details t-cell">
+                                   <div className="sub-container">
+                                       <div className="t-row">
+                                           <div className="track-label t-cell">
+                                               <a href={track.uri}>{track.label}</a>
+                                           </div>
+                                           <div className="sc-logo t-cell">
+                                               <div className="stats">
+                                                   {trackIconClass.call( this )} {trackPlaybackDelta.call( this )}
+                                               </div>
+                                           </div>
+                                       </div>
+                                       <div className="t-row">
+                                           <div className="track-name t-cell">
+                                               <div className="truncate">
+                                                   <a href={track.uri}>{track.name}</a>
+                                               </div>
+                                           </div>
+                                           <div className="track-actions t-cell">
+                                               &nbsp;
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+
                            </div>
-                           <div className="col-md-11 col-sm-10 col-xs-10 details clearfix">
-                               <div className="row">
-                                   <div className="col-md-12 col-sm-12 col-xs-12">
-                                       <div className="track-title">
-                                           { track.rank_playback_count } - { track.name }
-                                       </div>
-                                   </div>
-                               </div>
-                               <div className="row">
-                                   <div className="col-md-1 col-sm-1 col-xs-2">
-                                       <div className="play-indicator">
-                                           <span className="fa-stack fa-lg">
-                                               <i className="fa fa-circle fa-stack-1x"></i>
-                                               { playerControl }
-                                           </span>
-                                       </div>
-                                   </div>
-                                   <div className="col-md-11 col-sm-11 col-xs-10 waveform">
-                                       <img src={track.waveform_url} />
-                                   </div>
-                               </div>
-                               <div className="stats">
-                                   {trackIconClass.call( this )} {trackPlaybackDelta.call( this )}
-                               </div>
+
+                       </div>
+
+                       <div className="t-row">
+                           <div className="waveform t-cell">
+                               <img src={track.waveform_url} />
+                           </div>
+                       </div>
+
+                       <div className="t-row">
+                           <div className="track-stats t-cell">
+                               {track.download_count > 0 && <span><i className="fa fa-cloud-download"></i> {Number(track.download_count).toLocaleString()} &nbsp;</span>}
+                               <i className="fa fa-heart"></i> {Number(track.favoritings_count).toLocaleString()} &nbsp;
+                               <i className="fa fa-play"></i> {Number(track.playback_count).toLocaleString()}
                            </div>
                        </div>
                    </div>
